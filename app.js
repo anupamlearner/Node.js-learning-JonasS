bd556edfbe5511af15f3019fac555da033fbee61
anupamlearner <anupam.learn@gmail.com>
2024-03-28 19:54:47 +0545

    comments

--- app.js
+++ app.js
@@ -1,45 +1,55 @@
-const express = require("express");
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
+const express = require("express"); // Importing express library
+const morgan = require("morgan"); // Importing morgan library for logging
+const rateLimit = require("express-rate-limit"); // Importing rate-limiting middleware
+const helmet = require("helmet"); // Importing helmet for security headers
+const mongoSanitize = require("express-mongo-sanitize"); //Importing mongo-sanitize for data sanitization
+const xss = require("xss-clean"); // Importing
 
-const globalErrorHandler = require("./Controllers/errorController");
-const AppError = require("./utils/appError");
-const tourRouter = require("./Routes/tourRoutes");
-const userRouter = require("./Routes/userRoutes");
+const errorHandler = require("./Controllers/errorController"); // Importing error handling controller
+const AppError = require("./utils/appError"); // Importing custom error class
+const tourRoutes = require("./Routes/tourRoutes"); // Importing tour routes
+const userRoutes = require("./Routes/userRoutes"); // Importing user routes
 
-const app = express();
+const app = express(); // Creating express application
 
-// Global Middlewares
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "too many requests from this IP, please try again in an hour",
+// Set Security HTTP Header
+app.use(helmet({ xssFilter: true })); // Applying helmet middleware for security headers
+
+// Set request rate limiting
+const requestLimiter = rateLimit({
+  max: 100, // Maximum allowed requests
+  windowMs: 60 * 60 * 1000, // Time window for maximum requests
+  message: "Too many requests from this IP, please try again in an hour", // Error message for exceeding limit
 });
-app.use("/api", limiter);
+app.use("/api", requestLimiter); // Applying rate limiting middleware for all routes under '/api'
 
+// Development Logging
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+  // Check if environment is development
+  app.use(morgan("dev")); // Use morgan middleware for logging in development environment
 }
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 
+// Body Parser Middleware: parsing request data into JSON format
+app.use(express.json()); // Parsing JSON request bodies
+app.use(express.static(`${__dirname}/public`)); // Serving static files from the 'public' directory
+
+// Data sanitazitation against NoSQL query injection
+app.use(mongoSanitize());
+
+// Middleware to add request time to request object
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-
-  next();
+  req.requestTime = new Date().toISOString(); // Adding request time to request object
+  next(); // Move to the next middleware
 });
 
-// Routes ------------------------------
-app.use("/api/v1/tours", tourRouter); //This is a middleware
-app.use("/api/v1/users", userRouter); //This is a middleware
+// Routes
+app.use("/api/v1/tours", tourRoutes); // Using tour routes
+app.use("/api/v1/users", userRoutes); // Using user routes
 
-// Unhandled routers
+// Handling unhandled routes
 app.all("*", (req, res, next) => {
-  // const err = new Error(`Cannot find \< ${req.originalUrl} \> on this server`);
-  // err.status = "fail";
-  // err.statusCode = 404;
-  next(new AppError(`Cannot find \< ${req.originalUrl} \> on this server`));
+  next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404)); // Creating and passing error to next middleware
 });
 
-app.use(globalErrorHandler);
-module.exports = app;
+app.use(errorHandler); // Global error handling middleware
+module.exports = app; // Exporting the express application
