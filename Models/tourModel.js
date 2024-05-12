15294350d14f975ba8d9f0ebc88862a01444340b
anupamlearner <anupam.learn@gmail.com>
2024-05-12 20:02:17 +0545

    populating DB
    starting chapter

--- Models/tourModel.js
+++ Models/tourModel.js
@@ -118,6 +118,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+    ],
   },
 
   // Schema Options --------------------------------
